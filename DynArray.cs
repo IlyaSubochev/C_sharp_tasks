831af1b2e64160304540e1f4ecf7afd649aad093
IlyaSubochev <48482840+IlyaSubochev@users.noreply.github.com>
2019-09-05 09:52:22 +0300

    Update DynArray.cs

--- DynArray.cs
+++ DynArray.cs
@@ -59,7 +59,7 @@ namespace AlgorithmsDataStructures
 
         public void Insert(T itm, int index)
         {
-            if (count-1 == index || count==0 || count == index)
+            if (count == index )
             {
                Append(itm);
             }
