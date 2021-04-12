3719e56332097c02213d45063eabd112e5b722f6
IlyaSubochev <48482840+IlyaSubochev@users.noreply.github.com>
2021-04-12 15:48:52 +0300

    Update SortLevel.cs

--- SortLevel.cs
+++ SortLevel.cs
@@ -5,6 +5,7 @@ namespace SortSpace
 {
     public static class SortLevel
     {
+        #region 1. Сортировка (базовое)
         public static void SelectionSortStep(int[] array, int i) // Сортировка выбором
         {
             if (i < array.Length - 1)
@@ -52,5 +53,29 @@ namespace SortSpace
         {
             return a > b;
         }
+        #endregion
+
+        #region 2. Сортировка вставками
+
+        // вызов InsertionSortStep( [1,6,5,4,3,2,7], 3, 1 ) изменит входной массив на [1,3,5,4,6,2,7]
+        public static void InsertionSortStep(int[] array, int t, int i)
+        {
+            if (t <= 0) return;
+            InsertionSortCycle(array, t, i);
+        }
+
+        public static bool InsertionSortCycle(int[] array, int t, int i)
+        {
+            if (i + t >= array.Length) 
+                return true;
+            if (Compare(array[i], array[i + t]) && SwapElements(array, i, i + t)) 
+                return InsertionSortCycle(array, t, i + t) && false;
+            return InsertionSortCycle(array, t, i + t);
+        }
+        #endregion
+
+        #region 3. Сортировка Шелла
+
+        #endregion
     }
 }
